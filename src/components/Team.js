d4a91b05fb70774e5017cee9e773d403210d16ea
luisbryandr <luisbdominguezr@gmail.com>
2023-02-08 16:42:00 -0500

    added a local image as placeholder for team logo in Team component

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,11 +2,14 @@ import UpcomingGames from './UpcomingGames';
 import NewsHeadlines from './NewsHeadlines';
 import MiscInfo from './MiscInfo';
 
+import teamLogo from '../assets/Images/teamLogo.png';
+
 
 
 export default function Team () {
     return (
         <div>
+            <img src={teamLogo} alt="sample team's logo" />
             <UpcomingGames />
             <NewsHeadlines />
             <MiscInfo />
