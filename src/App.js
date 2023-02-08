c7cf2eda2411f56504b094b5929fcdc9f1f0fc08
luisbryandr <luisbdominguezr@gmail.com>
2023-02-08 16:17:18 -0500

    built out and conneted Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+/* Styles import here */ 
 import './App.css';
+
+/* Importing dependencies / libraries here */
+ /* Import components  */
+import Header from './components/Header';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Track your favorite Teams!</h1>
+     <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Header () {
+    return (
+        <h1>Custom follow your teams here</h1>
+    )
+}
