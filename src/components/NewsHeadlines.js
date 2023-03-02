ec7571a012b5654539fde3075852f6a772634972
luisbryandr <luisbdominguezr@gmail.com>
2023-03-02 18:52:28 -0500

    quick simulation rendering data, placeholder for data we will be consuming from sports api

--- src/components/NewsHeadlines.js
+++ src/components/NewsHeadlines.js
@@ -2,6 +2,8 @@ export default function NewsHeadlines () {
     return (
         <div className="container-newsheadlines">
             <h3>Latest Team's News</h3>
+            <p>This should be clickable and lead to the news article</p>
+            <p>Will probably need a news api or new section of an api</p>
         </div>
     )
 }
--- src/components/UpcomingGames.js
+++ src/components/UpcomingGames.js
@@ -1,7 +1,19 @@
+const dummyData = [
+    {
+        "id":1,
+        "team": 'Chelsea',
+        "date": "March 3rd 2023"
+    }
+]
+
+
 export default function UpcomingGames () {
     return (
         <div>
             <h3>The next upcoming matches are below:</h3>
+            <p>Game 1: {dummyData[0].team}  on {dummyData[0].date}</p>
+            <p>Game 2 vs AC Milan</p>
+            <p>Game 3 vs Porto</p>
         </div>
     )
     
