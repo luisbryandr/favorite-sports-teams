4a83b2ad41c559e8b11e2de7fdd2f65cabb4749a
luisbryandr <luisbdominguezr@gmail.com>
2023-03-02 18:40:26 -0500

    starting styling, grabbed and ordered singular team container div

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,13 +2,11 @@ import UpcomingGames from './UpcomingGames';
 import NewsHeadlines from './NewsHeadlines';
 import MiscInfo from './MiscInfo';
 
-import teamLogo from '../assets/Images/teamLogo.png';
-
-
+import teamLogo from '../assets/Images/teamLogo.png'
 
 export default function Team () {
     return (
-        <div>
+        <div className='team-container'>
             <img src={teamLogo} alt="sample team's logo" />
             <UpcomingGames />
             <NewsHeadlines />
@@ -17,3 +15,5 @@ export default function Team () {
         
     )
 }
+
+
