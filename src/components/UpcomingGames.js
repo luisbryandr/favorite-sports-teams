4cd2aa847f872d74a4432066b8048c0e779ded64
luisbryandr <luisbdominguezr@gmail.com>
2023-03-03 16:25:59 -0500

    succesfully rendering specific fixture data from api

--- src/components/UpcomingGames.js
+++ src/components/UpcomingGames.js
@@ -1,48 +1,33 @@
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
 
 //will need to move to a separate file
-const FIXTURES_URL = 'https://v3.football.api-sports.io/fixtures?season=2022&team=529&next=3'
+const FIXTURES_URL = ' https://v3.football.api-sports.io/fixtures?season=2022&team=529&next=3'
 const API_REQUEST_HEADERS = {
-    'X-RapidAPI-Host': 'v3.football.api-sports.io'
-    , 'X-RapidAPI-Key': '953200a1e98efd99b0adee31b6175653'
-  };
-const upcomingFixtures = axios.get( FIXTURES_URL, {
-    headers: API_REQUEST_HEADERS
-    // params: {
-    //     season: 2022,
-    //     team:529,
-    //     next:3
-    // }
-} )
-    .then( res => {
-     return res.data.response
-    } )
-    .catch(err => console.log(err))
+    'X-RapidAPI-Host': 'v3.football.api-sports.io',
+    'x-rapidapi-key': '953200a1e98efd99b0adee31b6175653'
+};
 
-const dummyData = [
-    {
-        "id":1,
-        "team": 'Chelsea',
-        "date": "March 3rd 2023"
-    },
-    {
-        "id":2,
-        "team": "AC Milan",
-        "date": "March 10th, 2023"
-    }
+export default function UpcomingGames() {
+    const [upcomingGames, setUpcomingGames] = useState([]);
 
-]
+    useEffect(() => {
+        axios.get(FIXTURES_URL, { headers: API_REQUEST_HEADERS })
+            .then(res => {
+                setUpcomingGames(res.data.response)
+            })
+            .catch(err => console.error(err))
+    }, [])
 
-console.log(upcomingFixtures);
 
-export default function UpcomingGames () {
     return (
         <div>
-            <h3>UpcomingGames:</h3>
-            {dummyData.map(details => {
-                return <p>vs {details.team} on {details.date}</p>
+            <h3>Upcoming Games:</h3>
+            {upcomingGames && upcomingGames.map((game, idx) => {
+                return <p key={idx}> {game.teams.home.name} vs {game.teams.away.name}</p>;
             })}
         </div>
     )
-    
+
 }
