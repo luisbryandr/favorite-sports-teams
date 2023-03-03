db2411af368c47c4fb6178346909e491223fdc9c
luisbryandr <luisbdominguezr@gmail.com>
2023-03-02 19:23:03 -0500

    refactor on Upcoming Games component to dynamically swift through data and displaying

--- src/components/UpcomingGames.js
+++ src/components/UpcomingGames.js
@@ -3,7 +3,13 @@ const dummyData = [
         "id":1,
         "team": 'Chelsea',
         "date": "March 3rd 2023"
+    },
+    {
+        "id":2,
+        "team": "AC Milan",
+        "date": "March 10th, 2023"
     }
+
 ]
 
 
@@ -11,9 +17,9 @@ export default function UpcomingGames () {
     return (
         <div>
             <h3>The next upcoming matches are below:</h3>
-            <p>Game 1: {dummyData[0].team}  on {dummyData[0].date}</p>
-            <p>Game 2 vs AC Milan</p>
-            <p>Game 3 vs Porto</p>
+            {dummyData.map(details => {
+                return <p>vs {details.team} on {details.date}</p>
+            })}
         </div>
     )
     
